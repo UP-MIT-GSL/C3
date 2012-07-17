6a3192009f0940df4d5931c363def43fda0f1afa
Lex Javier <lex.javier@gmail.com>
2012-07-17 19:22:47 +0800

    deleted heroku application

--- checkupccc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',    
-    url(r'^$', 'checklist.views.index'),
-    url(r'^admin/', include(admin.site.urls)),
-)
